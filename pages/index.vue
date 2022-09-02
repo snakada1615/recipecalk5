bc923a8f03212c9431a96ea8b0cafe9547b25ccc
shunichi <khf04440@nifty.com>
2022-09-02 15:08:27 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col class="bg-danger-8">
+        <div class="text-white">
+          halo
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
