cdb390acd38874b77cd5a578e1ccba46d247ef4d
shunichi <khf04440@nifty.com>
2022-09-05 00:38:20 +0900

    fireStrore, firebase-hostingを設定

---
+++ pages/testInitial.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-container>
+    test
+    <div>{{ firstName }}</div>
+  </b-container>
+</template>
+<script>
+import { fireGetDoc } from '~/plugins/firebasePlugin'
+
+export default {
+  async asyncData () {
+    const res = await fireGetDoc('testDb', 'fireDbFirstTest')
+    return {
+      firstName: res.firstName
+    }
+  }
+}
+</script>
